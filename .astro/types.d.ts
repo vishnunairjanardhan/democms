084a34af5b7a5fcdf87003eb9b71eabfc59ae74c
NikhilDandekar <nikhildandekar138@gmail.com>
2024-01-29 17:51:00 +0530

    Add AI blogs

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -184,42 +184,77 @@ declare module 'astro:content' {
   slug: "1";
   body: string;
   collection: "posts";
-  data: InferEntrySchema<"posts">
+  data: any
+} & { render(): Render[".md"] };
+"16.md": {
+	id: "16.md";
+  slug: "16";
+  body: string;
+  collection: "posts";
+  data: any
+} & { render(): Render[".md"] };
+"17.md": {
+	id: "17.md";
+  slug: "17";
+  body: string;
+  collection: "posts";
+  data: any
+} & { render(): Render[".md"] };
+"18.md": {
+	id: "18.md";
+  slug: "18";
+  body: string;
+  collection: "posts";
+  data: any
+} & { render(): Render[".md"] };
+"19.md": {
+	id: "19.md";
+  slug: "19";
+  body: string;
+  collection: "posts";
+  data: any
 } & { render(): Render[".md"] };
 "2.md": {
 	id: "2.md";
   slug: "2";
   body: string;
   collection: "posts";
-  data: InferEntrySchema<"posts">
+  data: any
+} & { render(): Render[".md"] };
+"20.md": {
+	id: "20.md";
+  slug: "20";
+  body: string;
+  collection: "posts";
+  data: any
 } & { render(): Render[".md"] };
 "3.md": {
 	id: "3.md";
   slug: "3";
   body: string;
   collection: "posts";
-  data: InferEntrySchema<"posts">
+  data: any
 } & { render(): Render[".md"] };
 "4.md": {
 	id: "4.md";
   slug: "4";
   body: string;
   collection: "posts";
-  data: InferEntrySchema<"posts">
+  data: any
 } & { render(): Render[".md"] };
 "6.md": {
 	id: "6.md";
   slug: "6";
   body: string;
   collection: "posts";
-  data: InferEntrySchema<"posts">
+  data: any
 } & { render(): Render[".md"] };
 "7.md": {
 	id: "7.md";
   slug: "7";
   body: string;
   collection: "posts";
-  data: InferEntrySchema<"posts">
+  data: any
 } & { render(): Render[".md"] };
 };
 "posts-old": {
@@ -282,5 +317,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	type ContentConfig = typeof import("../src/content/config");
+	type ContentConfig = never;
 }
