a87715ad38acaae0f35e677086f5d2752dd0e346
Mrudul Tarwatkar <mrudultarwatkar@gmail.com>
2024-03-12 13:19:08 +0530

    Missing packages added to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,6 @@ export default defineConfig({
     skipInline: false,
     drafts: true,
   },
-   site: 'https://lexingtonthemes.com',
+  site: 'https://lexingtonthemes.com',
   integrations: [tailwind(), react(), sitemap(), mdx()]
 });
