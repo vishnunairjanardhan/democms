3d60dc5f94ca18097e4fe68d51b4901bb415aa28
PiyushSingh2212@gmail.com <piyushsingh2212@gmail.com>
2024-06-26 16:42:10 +0530

    Added blog page

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -514,13 +514,6 @@ declare module 'astro:content' {
   collection: "posts";
   data: any
 } & { render(): Render[".md"] };
-"ecommerce-referral-marketing-strategies.md": {
-	id: "ecommerce-referral-marketing-strategies.md";
-  slug: "ecommerce-referral-marketing-strategies";
-  body: string;
-  collection: "posts";
-  data: any
-} & { render(): Render[".md"] };
 "ecommerce-strategy-guide.md": {
 	id: "ecommerce-strategy-guide.md";
   slug: "ecommerce-strategy-guide";
@@ -1280,6 +1273,13 @@ declare module 'astro:content' {
 "what-is-omnichannel-commerce.md": {
 	id: "what-is-omnichannel-commerce.md";
   slug: "what-is-omnichannel-commerce";
+  body: string;
+  collection: "posts";
+  data: any
+} & { render(): Render[".md"] };
+"what-is-referral-marketing.md": {
+	id: "what-is-referral-marketing.md";
+  slug: "what-is-referral-marketing";
   body: string;
   collection: "posts";
   data: any
