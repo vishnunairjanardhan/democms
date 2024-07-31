6ea813f15284da95e07d8a4008d87f3cf647d0d1
Atish <atishjadhav835@gmail.com>
2024-07-31 18:33:57 +0530

    Started working on Paytronix Integration Page

--- src/config/Integration.jsx
+++ src/config/Integration.jsx
@@ -107,6 +107,13 @@ export const POS = [
   desc: "99minds integrates with iPaaS.com to enhance commerce experiences by seamlessly syncing loyalty data across CRM, ERP, eCommerce, FSM, Marketing, POS, and Review Management systems, enabling unified, real-time management of loyalty programs from a single platform.",
   url: "/integrations/ipaas",
   alt:"99minds-IPaaS Integration"
+}, 
+{
+  image: '/integrations/hike-icon.svg',
+  title: 'Paytronix',
+  desc: "99minds integrates with Paytronix POS to create an omnichannel experience for gift cards, loyalty programs, store credits, and coupons.  Customers can manage their programs and redeem rewards seamlessly across your online store and your Hike POS system.",
+  url: "/integrations/paytronix",
+  alt:"99minds-Paytronix Integration"
 }
 ]
 
