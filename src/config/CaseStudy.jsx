a8fa50054509c9b1aad3394d41c8e3745e9b253b
Vishnu <vjnair2001@gmail.com>
2024-08-07 09:31:16 +0530

    changes in config file

--- src/config/CaseStudy.jsx
+++ src/config/CaseStudy.jsx
@@ -1,22 +1,22 @@
 export const buttonConfig = [
     { label: 'All', icon: 'icon-tabler-components', id: 'tabs-5-1' },
     { label: 'Fashion', icon: 'icon-tabler-components', id: 'tabs-5-2' },
-    //{ label: 'Sports', icon: 'icon-tabler-components', id: 'tabs-5-3' },
-    //{ label: 'Food & Beverages', icon: 'icon-tabler-components', id: 'tabs-5-4' },
-    //{ label: 'Beauty', icon: 'icon-tabler-components', id: 'tabs-5-5' },
-    //{ label: 'Jewellery', icon: 'icon-tabler-components', id: 'tabs-5-6' },
-    //{ label: 'Other', icon: 'icon-tabler-components', id: 'tabs-5-7' },
+    { label: 'Sports', icon: 'icon-tabler-components', id: 'tabs-5-3' },
+    { label: 'Food & Beverages', icon: 'icon-tabler-components', id: 'tabs-5-4' },
+    { label: 'Beauty', icon: 'icon-tabler-components', id: 'tabs-5-5' },
+    { label: 'Jewellery', icon: 'icon-tabler-components', id: 'tabs-5-6' },
+    { label: 'Other', icon: 'icon-tabler-components', id: 'tabs-5-7' },
   ];
 
 export const cardData =[
     {date:'April 2024', title:'Flynnohara', description:'99minds joins forces with FlynnOhara to Outfit 1,500 Schools and 500,000 Students with Quality Uniforms!', href:"/case-study/flynnohara", img:"/assets/case-study/Flynnohara-CS1.jpg", alt:"Flynnohara Case Study"},
     // {date:'April 2024', title:'Birdsnest', description:'Birdsnest Migrates to ConnectPOS with 99minds to Enhance Omnichannel Experience', href:"/case-study/birdsnest", img:"/assets/case-study/Birdnest-case-study.jpg", alt:"Birdsnest Case Study"},
-    {date:'April 2024', title:'Solstice Sun Glasses', description:'99minds Joins Forces with Solstice Sunglasses to Generate Over $300K in Revenue through Omnichannel Gift Card Sales!', href:'/case-study/solsticesunglasses', img:"/assets/case-study/Solstice-CS.jpg", alt:"Solstice Sun Glasses Case Study"},
+    // {date:'April 2024', title:'Solstice Sun Glasses', description:'99minds Joins Forces with Solstice Sunglasses to Generate Over $300K in Revenue through Omnichannel Gift Card Sales!', href:'/case-study/solsticesunglasses', img:"/assets/case-study/Solstice-CS.jpg", alt:"Solstice Sun Glasses Case Study"},
 ]
 
 export const Fashion =[
     {date:'April 2024', title:'Flynnohara', description:'99minds joins forces with FlynnOhara to Outfit 1,500 Schools and 500,000 Students with Quality Uniforms!', href:"/case-study/flynnohara", img:"/assets/case-study/Flynnohara-CS1.jpg", alt:"Flynnohara Case Study"},
-    {date:'April 2024', title:'Solstice Sun Glasses', description:'99minds Joins Forces with Solstice Sunglasses to Generate Over $300K in Revenue through Omnichannel Gift Card Sales!', href:'/case-study/solsticesunglasses', img:"/assets/case-study/Solstice-CS.jpg", alt:"Solstice Sun Glasses Case Study"},
+    // {date:'April 2024', title:'Solstice Sun Glasses', description:'99minds Joins Forces with Solstice Sunglasses to Generate Over $300K in Revenue through Omnichannel Gift Card Sales!', href:'/case-study/solsticesunglasses', img:"/assets/case-study/Solstice-CS.jpg", alt:"Solstice Sun Glasses Case Study"},
 ]
 
 export const Others =[
