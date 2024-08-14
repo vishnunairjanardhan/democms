cb85c629cd2dae0a8ffd5b4ae3683e790a44ffc8
Atish <atishjadhav835@gmail.com>
2024-08-14 17:49:23 +0530

    Added Fiserv Integration Page

--- src/config/Integration.jsx
+++ src/config/Integration.jsx
@@ -208,9 +208,10 @@ export const Payment = [
 export const Giftcard = [
 {
   image: '/integrations/fiserve.svg',
-  title: 'Fiserve',
+  title: 'Fiserv',
   desc: 'A comprehensive gift card solution that seamlessly integrates with 99minds. Fiserv handles all aspects of gift card issuance, from initial creation to fulfillment, allowing you to manage your program efficiently within the 99minds platform.',
-  alt:"99minds-Finserve Integration"
+  url: "/integrations/fiserv",
+  alt:"99minds-Fiserv Integration"
 },
 {
   image: '/integrations/shift4.svg',
@@ -234,7 +235,6 @@ export const Giftcard = [
   image: '/integrations/paytronix.svg',
   title: 'Paytronix',
   desc: 'A specialized gift card and loyalty solution that integrates with 99minds, particularly designed for restaurants.  Paytronix allows you to leverage 99minds for issuing gift cards with features specifically tailored to the restaurant industry, such as point-of-sale integration and loyalty program rewards.',
-  url: "/integrations/paytronix",
   url: "/integrations/paytronix",
   alt:"99minds-Paytronix Integration"
 }
