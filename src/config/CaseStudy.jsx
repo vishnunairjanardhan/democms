b787a63c8bbf9c777b26cf402701d93e9c1f19bf
NikhilDandekar <nikhildandekar138@gmail.com>
2024-10-16 17:11:17 +0530

    Testimonial changes updated

--- src/config/CaseStudy.jsx
+++ src/config/CaseStudy.jsx
@@ -9,7 +9,7 @@ export const buttonConfig = [
   ];
 
 export const cardData =[
-    {date:'April 2024', title:'Flynnohara', description:'99minds joins forces with FlynnOhara to Outfit 1,500 Schools and 500,000 Students with Quality Uniforms!', href:"/case-study/flynnohara", img:"/assets/case-study/Flynnohara-CS1.jpg", alt:"Flynnohara Case Study"},
+    {date:'April 2024', title:'Flynnohara', description:'99minds joins forces with FlynnOhara to Outfit 1,500 Schools and 500,000 Students with Quality Uniforms!', href:"/case-study/flynnohara", img:"/assets/case-study/flynnohara_1.webp", alt:"Flynnohara Case Study"},
     {date:'October 2024', title:'Natori', description:'99Minds Helps Natori Enhance Sales and Customer Experience with Seamless Gift Card Integration', href:"/case-study/natori", img:"/assets/case-study/natori4.png", alt:"Natori Case Study"},
     //{date:'April 2024', title:'Solstice Sun Glasses', description:'99minds Joins Forces with Solstice Sunglasses to Generate Over $300K in Revenue through Omnichannel Gift Card Sales!', href:'/case-study/solsticesunglasses', img:"/assets/case-study/Solstice-CS.jpg", alt:"Solstice Sun Glasses Case Study"},
     //{date: 'April 2024', heading:'HEXCLAD', title:'$450k within first 90 days', description:'Hexclad partnered with 99minds to launch their tiered loyalty program', href:'/case-study/hexclad', img:"/assets/case-study/hexclad-1.webp", alt:"Hexclad"},
