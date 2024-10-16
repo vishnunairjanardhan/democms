b92788df38a4346b52f46673cf8615833b5c3a58
Atish <atishjadhav835@gmail.com>
2024-10-16 12:27:37 +0530

    Updated all case studies

--- src/config/CaseStudy.jsx
+++ src/config/CaseStudy.jsx
@@ -11,8 +11,13 @@ export const buttonConfig = [
 export const cardData =[
     {date:'April 2024', title:'Flynnohara', description:'99minds joins forces with FlynnOhara to Outfit 1,500 Schools and 500,000 Students with Quality Uniforms!', href:"/case-study/flynnohara", img:"/assets/case-study/Flynnohara-CS1.jpg", alt:"Flynnohara Case Study"},
     {date:'October 2024', title:'Natori', description:'99Minds Helps Natori Enhance Sales and Customer Experience with Seamless Gift Card Integration', href:"/case-study/natori", img:"/assets/case-study/natori4.png", alt:"Natori Case Study"},
-    {date:'October 2024', title:'Solstice Sun Glasses', description:'99minds Joins Forces with Solstice Sunglasses to Generate Over $300K in Revenue through Omnichannel Gift Card Sales!', href:'/case-study/solsticesunglasses', img:"/assets/case-study/Solstice-CS.jpg", alt:"Solstice Sun Glasses Case Study"},
-    {date:'October 2024', title:'Birdsnest', description:'Birdsnest’s Omnichannel Gift Card Integration with ConnectPOS and BigCommerce', href:'/case-study/solsticesunglasses', img:"/assets/case-study/birdsnest.png", alt:"Birdsnest Case Study"},
+    {date:'October 2024', title:'Solstice Sun Glasses', description:'99minds Joins Forces with Solstice Sunglasses to Generate Over $300K in Revenue through Omnichannel Gift Card Sales!', href:'/case-study/solsticesunglasses', img:"/assets/case-study/solstice-banner.webp", alt:"Solstice Sun Glasses Case Study"},
+    {date:'October 2024', title:'Birdsnest', description:'Birdsnest’s Omnichannel Gift Card Integration with ConnectPOS and BigCommerce', href:'/case-study/birdsnest', img:"/assets/case-study/birdsnest.png", alt:"Birdsnest Case Study"},
+    {date:'October 2024', title:'Mudaustralia', description:"MudAustralia's Omnichannel & Multi-Currency Gift card Transformation leads to growth", href:'/case-study/mudaustralia', img:"/assets/case-study/mud-Aus-banner.svg", alt:"MudAustralia Case Study"},
+    {date:'October 2024', title:'Cariloha', description:"Cariloha’s Social Media Gift Card Promotion Drives Customer Acquisition and Engagement", href:'/case-study/cariloha', img:"/assets/case-study/cariloha-banner1.webp", alt:"Cariloha Case Study"},
+    {date:'October 2024', title:'Jaxengrey', description:"Jaxen Grey's Social Media Gift card Promotions Drive Engagement and Sales", href:'/case-study/jaxengrey', img:"/assets/case-study/jaxengrey2.png", alt:"Jaxengrey Case Study"},
+    {date:'October 2024', title:'Bestop', description:"Bestop’s Gift Card Facebook Promotion with Heather Storm Off-Road Series", href:'/case-study/bestop', img:"/assets/case-study/Bestop-banner.webp", alt:"Bestop Case Study"},
+    {date:'October 2024', title:'TasselsShoes', description:"TasselsShoes.com’s Omnichannel Store Credit, Loyalty, and Gift Card Integration with Klaviyo", href:'/case-study/tasselsshoes', img:"/assets/case-study/tassels1.png", alt:"TasselsShoes Case Study"},
     
 ]
 
