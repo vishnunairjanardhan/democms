5b0a7f1f26978c899dc3a66fb0e3e4e1639b2b6d
NikhilDandekar <nikhildandekar138@gmail.com>
2024-10-30 14:54:54 +0530

    Shopifycheckout component updated

--- src/config/component.jsx
+++ src/config/component.jsx
@@ -1,52 +1,63 @@
 export const rewards = [
+    
     {
-        "logo": " assets/homepage/GC.svg",
-        "line1": "Like page on Facebook",
-        "line2": "50 Points"
-    },
-    {
-        "logo": " assets/homepage/GC.svg",
+        "logo": " assets/shopifycheckout/order.svg",
         "line1": "Place an order",
         "line2": "Multiple Points for every $1 spent"
     },
     {
-        "logo": " assets/homepage/GC.svg",
+        "logo": " assets/shopifycheckout/signup.svg",
         "line1": "Sign up",
         "line2": "500 Points"
     },
     {
-        "logo": " assets/homepage/GC.svg",
-        "line1": "Share link on Facebook",
-        "line2": "52 Points"
-    },
-    {
-        "logo": " assets/homepage/GC.svg",
-        "line1": "Follow on Instagram",
-        "line2": "50 Points"
-    },
-    {
-        "logo": " assets/homepage/GC.svg",
+        "logo": " assets/shopifycheckout/newsletter.svg",
         "line1": "Subscribe to a newsletter",
         "line2": "50 Points"
     },
     {
-        "logo": " assets/homepage/GC.svg",
-        "line1": "Share link on Twitter",
-        "line2": "50 Points"
-    },
-    {
-        "logo": " assets/homepage/GC.svg",
+        "logo": " assets/shopifycheckout/product-review.svg",
         "line1": "Post a product review",
         "line2": "500 Points"
     },
     {
-        "logo": " assets/homepage/GC.svg",
+        "logo": " assets/shopifycheckout/referral.svg",
+        "line1": "Complete a Referral",
+        "line2": "100 Points"
+    }
+]
+
+export const rewards1 = [
+   
+    {
+        "logo": " assets/shopifycheckout/fb.svg",
+        "line1": "Share link on Facebook",
+        "line2": "25 Points"
+    },
+    {
+        "logo": " assets/shopifycheckout/instagram.svg",
+        "line1": "Follow on Instagram",
+        "line2": "50 Points"
+    },
+    {
+        "logo": " assets/shopifycheckout/youtube.svg",
+        "line1": "Subscribe on Youtube",
+        "line2": "50 Points"
+    },
+    {
+        "logo": " assets/shopifycheckout/tiktok.svg",
         "line1": "Follow on TikTok",
         "line2": "50 Points"
     },
     {
-        "logo": " assets/homepage/GC.svg",
-        "line1": "Complete a Referral",
-        "line2": "100 Points"
-    }
+        "logo": " assets/shopifycheckout/twitter.svg",
+        "line1": "Share link on Twitter",
+        "line2": "50 Points"
+    },
+    {
+        "logo": " assets/shopifycheckout/like.svg",
+        "line1": "Like page on Facebook",
+        "line2": "50 Points"
+    },
+    
 ]
