87bf37a453aefaf0a0ef7753e82a0a24091a1b39
Atish <atishjadhav835@gmail.com>
2024-12-13 15:58:38 +0530

    Removed animation packages

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -325,13 +325,6 @@ declare module 'astro:content' {
   collection: "posts";
   data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
-"Black-friday-promotion-ideas.md": {
-	id: "Black-friday-promotion-ideas.md";
-  slug: "black-friday-promotion-ideas";
-  body: string;
-  collection: "posts";
-  data: InferEntrySchema<"posts">
-} & { render(): Render[".md"] };
 "Ways to Incorporate Gift Cards Into Your Customer Retention Strategy.md": {
 	id: "Ways to Incorporate Gift Cards Into Your Customer Retention Strategy.md";
   slug: "ways-to-incorporate-gift-cards-into-your-customer-retention-strategy";
@@ -426,6 +419,13 @@ declare module 'astro:content' {
 "black-friday-cyber-monday-record-success .md": {
 	id: "black-friday-cyber-monday-record-success .md";
   slug: "black-friday-cyber-monday-record-success-";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"black-friday-promotion-ideas.md": {
+	id: "black-friday-promotion-ideas.md";
+  slug: "black-friday-promotion-ideas";
   body: string;
   collection: "posts";
   data: InferEntrySchema<"posts">
