e89aa39b1f2a93d246d38a15bf08927696550c31
vishnunairjanardhan <vjnair2001@gmail.com>
2024-12-30 19:29:57 +0530

    Changes in ROI and Profit impact

--- src/config/RoiConfig.jsx
+++ src/config/RoiConfig.jsx
@@ -3,6 +3,8 @@ export const industries = [
     id: {
       name: "LITE",
       price: "$19",
+      value:19,
+      growth:5,
       imageSrc: "/assets/ROI/electronics.svg", // Replace with the actual image path or URL
     },
   },
@@ -10,6 +12,8 @@ export const industries = [
     id: {
       name: "BASIC",
       price: "$49",
+      growth:8,
+      value:49,
       imageSrc: "/assets/ROI/fashion.svg", // Replace with the actual image path or URL
     },
   },
@@ -17,6 +21,8 @@ export const industries = [
     id: {
       name: "PRO",
       price: "$99",
+      growth:15,
+      value:99,
       imageSrc: "/assets/ROI/beauty1.svg", // Replace with the actual image path or URL
     },
   },
@@ -24,6 +30,8 @@ export const industries = [
     id: {
       name: "ULTRA",
       price: "$199",
+      growth:20,
+      value:199,
       imageSrc: "/assets/ROI/home-decor.svg", // Replace with the actual image path or URL
     },
   },
@@ -31,6 +39,8 @@ export const industries = [
     id: {
       name: "PREMIUM",
       price: "$499",
+      growth:25,
+      value:499,
       imageSrc: "/assets/ROI/sports.svg", // Replace with the actual image path or URL
     },
   },
@@ -38,6 +48,8 @@ export const industries = [
     id: {
       name: "ENTERPRISE",
       price: "Custom",
+      growth:30,
+      value:1000,
       imageSrc: "/assets/ROI/food.svg", // Replace with the actual image path or URL
     },
   },
