744da37591f0feded4eea4326cc6dd90f1588428
Atish <atishjadhav835@gmail.com>
2025-02-04 12:34:14 +0530

    Added new marquee component

---
+++ src/components/testimonials/NewLogoCloud.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function NewLogoCloud() {
+  const images = [
+    { src: "../assets/logo-cloud/Pinstripe-b.svg", width: 160, alt: "Pinstripe logo" },
+    { src: "../assets/logo-cloud/Bestop-b.svg", width: 160, alt: "Bestop logo" },
+    { src: "../assets/logo-cloud/cornerbakery-b.svg", width: 160, alt: "Corner bakery logo" },
+    { src: "/../assets/logo-cloud/cariloha-b.svg", width: 160, alt: "Cariloha logo" },
+    { src: "/../assets/logo-cloud/birdsnest-b.svg", width: 160, alt: "Birdsnest logo" },
+    { src: "/../assets/logo-cloud/ThirdLove-b.svg", width: 160, alt: "Thirdlove Logo" },
+    { src: "../assets/logo-cloud/Flynnohara-b.svg", width: 160, alt: "Flynnohara Logo" },
+    { src: "../assets/logo-cloud/coolvines-b.svg", width: 160, alt: "Coolvines logo" },
+    { src: "../assets/logo-cloud/solsticesunglasses-b.svg", width: 160, alt: "Solstice logo" },
+    { src: "../assets/logo-cloud/solobrands-b.svg", width: 160, alt: "Solobrand logo" },
+  ];
+
+  const duplicateImages = [...images, ...images]; 
+
+  return (
+    <div className="overflow-hidden shadow-sm p-4 mt-4">
+      <div className="flex gap-4 items-center animate-marquee">
+        {duplicateImages.map((image, index) => (
+          <img
+            key={index}
+            role="img"
+            loading="lazy"
+            className="mx-auto justify-center px-4"
+            width={image.width}
+            alt={image.alt}
+            src={image.src}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default NewLogoCloud;
