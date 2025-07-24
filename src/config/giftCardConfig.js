d10f28f649e1bc97b4b3099974399750985aafd4
vishnunairjanardhan <vjnair2001@gmail.com>
2025-07-24 15:05:11 +0530

    Created New GiftCardWall page

---
+++ src/config/giftCardConfig.js
@@ -0,0 +1,33 @@
+export const countries = ['US', 'Canada', 'Europe', 'Australia'];
+
+export const giftCards = [
+  {
+    name: 'Amazon',
+    image: 'https://via.placeholder.com/300x200?text=Amazon',
+    url: 'https://www.amazon.com/gift-cards',
+    countries: ['US', 'Canada', 'Europe'],
+    genre: 'Shopping'
+  },
+  {
+    name: 'Starbucks',
+    image: 'https://via.placeholder.com/300x200?text=Starbucks',
+    url: 'https://www.starbucks.com/gift-cards',
+    countries: ['US', 'Australia'],
+    genre: 'Food & Drink'
+  },
+  {
+    name: 'Apple',
+    image: 'https://via.placeholder.com/300x200?text=Apple',
+    url: 'https://www.apple.com/shop/gift-cards',
+    countries: ['US', 'Europe', 'Australia'],
+    genre: 'Tech'
+  },
+  {
+    name: 'Target',
+    image: 'https://via.placeholder.com/300x200?text=Target',
+    url: 'https://www.target.com/gift-cards',
+    countries: ['US'],
+    genre: 'Retail'
+  }
+  // Add more cards here
+];
