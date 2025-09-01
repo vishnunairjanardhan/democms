2de0e50080c4a7399d13e43bfa6755a29da8c459
vishnunairjanardhan <vjnair2001@gmail.com>
2025-09-01 15:59:01 +0530

    feature tabs issue fix

--- src/components/features/FeartureTabs.jsx
+++ src/components/features/FeartureTabs.jsx
@@ -72,27 +72,6 @@ const TabbedImages = () => {
                   )
               )}
             </AnimatePresence>
-          <div className="relative flex justify-center w-full lg:min-h-[518px] md:min-h-[280px]">
-            <div className="relative w-full max-w-[1300px] h-auto overflow-hidden rounded-2xl mt-2 md:mt-6 lg:mt-0">
-              <AnimatePresence mode="sync">
-                {tabs.map(
-                  (tab, index) =>
-                    index === activeIndex && (
-                      <motion.img
-                        key={tab.id}
-                        src={tab.img}
-                        alt={tab.alt}
-                        loading="lazy"
-                        className="absolute inset-0 object-cover rounded-2xl"
-                        initial={{ opacity: 0, scale: 0.96 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        exit={{ opacity: 0, scale: 1.02 }}
-                        transition={{ duration: 0.5, ease: "easeInOut" }}
-                      />
-                    )
-                )}
-              </AnimatePresence>
-            </div>
           </div>
         </div>
       </div>
