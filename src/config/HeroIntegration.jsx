aa01583248845c98d21d1ae8560dfacc47cc78a0
NikhilDandekar <nikhildandekar138@gmail.com>
2025-10-09 16:32:04 +0530

    changes in navbar & integration pages

--- src/config/HeroIntegration.jsx
+++ src/config/HeroIntegration.jsx
@@ -28,3 +28,65 @@ export const shopifyConfig = {
     { label: "Learn More", link: "/learn" },
   ],
 };
+
+export const shopifyplusConfig = {
+  logo: {
+    src: "/assets/shopify/99minds-S+.svg",
+    alt: "99minds and Shopifyplus Integration",
+    width: 474,
+    height: 181,
+  },
+  heading: "Shopifyplus and 99minds Integration",
+  description:
+    "Countless Shopify stores trust 99minds for creating impactful Shopify loyalty programs and enhancing Shopify gift card experience. Integrate now to boost sales and customer retention.",
+  reviews: {
+    image: "/5-star.svg",
+    alt: "5 stars",
+    text: "5 Star Reviews",
+  },
+  cta: {
+    text: "Install on",
+    link: "https://apps.shopify.com/99minds-gift-card-loyalty-referral#",
+    logo: {
+      src: "/assets/shopify/Shopify-logo.svg",
+      alt: "Shopify logo",
+      width: 90,
+    },
+  },
+  showButtonsGrid: true,
+  buttons: [
+    { label: "Book Demo", link: "/demo" },
+    { label: "Learn More", link: "/learn" },
+  ],
+};
+
+export const bigcommerceConfig = {
+  logo: {
+    src: "/assets/bigcommerce/99minds-BC2.svg",
+    alt: "99minds and Bigcommerce Integration",
+    width: 474,
+    height: 181,
+  },
+  heading: "Bigcommerce and 99minds Integration",
+  description:
+    "Add 99minds app to your Bigcommerce store and develop robust loyalty programs and gift cards experience. Install today to enhance sales and customer loyalty.",
+  reviews: {
+    image: "/5-star.svg",
+    alt: "5 stars",
+    text: "5 Star Reviews",
+  },
+  cta: {
+    text: "Install on",
+    link: "https://www.bigcommerce.com/apps/99minds-gift-card/",
+    logo: {
+      src: "/assets/homepage/Bcom-l.svg",
+      alt: "Bigcommerce logo",
+      width: 90,
+    },
+  },
+  showButtonsGrid: true,
+  buttons: [
+    { label: "Book Demo", link: "/demo" },
+    { label: "Learn More", link: "/learn" },
+  ],
+};
