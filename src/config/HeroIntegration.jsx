b03b86fead943643fddc1a4332481fc77f0ae18f
NikhilDandekar <nikhildandekar138@gmail.com>
2025-10-10 15:26:40 +0530

    Ecom integration pages updated

--- src/config/HeroIntegration.jsx
+++ src/config/HeroIntegration.jsx
@@ -19,7 +19,7 @@ export const shopifyConfig = {
     logo: {
       src: "/assets/shopify/Shopify-logo.svg",
       alt: "Shopify logo",
-      width: 90,
+      width: 100,
     },
   },
   showButtonsGrid: true,
@@ -50,7 +50,7 @@ export const shopifyplusConfig = {
     logo: {
       src: "/assets/shopify/Shopify-logo.svg",
       alt: "Shopify logo",
-      width: 90,
+      width: 100,
     },
   },
   showButtonsGrid: true,
@@ -81,7 +81,100 @@ export const bigcommerceConfig = {
     logo: {
       src: "/assets/homepage/Bcom-l.svg",
       alt: "Bigcommerce logo",
-      width: 90,
+      width: 120,
+    },
+  },
+  showButtonsGrid: true,
+  buttons: [
+    { label: "Book Demo", link: "/demo" },
+    { label: "Learn More", link: "/learn" },
+  ],
+};
+
+export const woocommerceConfig = {
+  logo: {
+    src: "/assets/99minds-WC2.svg",
+    alt: "99minds and Woocommerce Integration",
+    width: 474,
+    height: 181,
+  },
+  heading: "Woocommerce and 99minds Integration",
+  description:
+    "Woocommerce stores depend on 99minds to develop robust loyalty programs and improve the gift card experience. Incorporate it today to increase your sales and enhance customer loyalty.",
+  reviews: {
+    image: "/5-star.svg",
+    alt: "5 stars",
+    text: "5 Star Reviews",
+  },
+  cta: {
+    text: "Install on",
+    link: "https://wordpress.org/plugins/99minds-gift-card/",
+    logo: {
+      src: "/assets/woocommerce/woocommerce-logo-1.svg",
+      alt: "Woocommerce logo",
+      width: 120,
+    },
+  },
+  showButtonsGrid: true,
+  buttons: [
+    { label: "Book Demo", link: "/demo" },
+    { label: "Learn More", link: "/learn" },
+  ],
+};
+
+export const salesforceConfig = {
+  logo: {
+    src: "/assets/salesforce/99minds-salesforce1.svg",
+    alt: "Salesforce and 99minds integration",
+    width: 474,
+    height: 181,
+  },
+  heading: "Salesforce and 99minds integration",
+  description:
+    "Salesforce has partnered with 99minds to help you build seamless gifting experience and profitable customer relationships with smart loyalty and rewards, online and in-store.",
+  reviews: {
+    image: "/5-star.svg",
+    alt: "5 stars",
+    text: "5 Star Reviews",
+  },
+  cta: {
+    text: "Install on",
+    link: "https://appexchange.salesforce.com/appxListingDetail?listingId=a0N4V00000J6Jc3UAF",
+    logo: {
+      src: "/assets/homepage/Salesforce-l.svg",
+      alt: "Salesforce logo",
+      width: 130,
+    },
+  },
+  showButtonsGrid: true,
+  buttons: [
+    { label: "Book Demo", link: "/demo" },
+    { label: "Learn More", link: "/learn" },
+  ],
+};
+
+export const appbrewConfig = {
+  logo: {
+    src: "/assets/appbrew/99minds-Appbrew.webp",
+    alt: "Appbrew and 99minds Integration",
+    width: 474,
+    height: 181,
+  },
+  heading: "Appbrew and 99minds Integration",
+  description:
+    "Countless Shopify stores trust 99minds for creating impactful Shopify loyalty programs and enhancing Shopify gift card experience. Integrate now to boost sales and customer retention.",
+  reviews: {
+    image: "/5-star.svg",
+    alt: "5 stars",
+    text: "5 Star Reviews",
+  },
+  cta: {
+    text: "Install on",
+    link: "https://apps.shopify.com/99minds-gift-card-loyalty-referral#",
+    logo: {
+      src: "/assets/appbrew/appbrew-logo.svg",
+      alt: "Appbrew logo",
+      width: 130,
     },
   },
   showButtonsGrid: true,
