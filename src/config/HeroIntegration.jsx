9ed863f31a53fe85ca9c490a6e5ff6ce84e79f7c
vishnunairjanardhan <vjnair2001@gmail.com>
2025-10-10 13:14:25 +0530

    changes in config

--- src/config/HeroIntegration.jsx
+++ src/config/HeroIntegration.jsx
@@ -51,3 +51,57 @@ export const shopifyConfig = {
     { label: "Learn More", link: "/learn" },
   ],}
 };
+
+export const shopifyPlusConfig = {
+  seo: {
+    title: "99minds and Shopify Plus integration | 99minds",
+    description: "Add the most powerful omnichannel gift card and loyalty program app to your Shopify account and improve customer retention and revenue.",
+    canonical: "https://www.99minds.io/integrations/shopifyplus",
+    openGraph: {
+      url: "https://www.99minds.io/integrations/shopifyplus",
+      title: "99minds and Shopify Plus integration | 99minds",
+      description: "Add the most powerful omnichannel gift card and loyalty program app to your Shopify account and improve customer retention and revenue.",
+      images: [
+        {
+          src: "https://www.99minds.io/assets/homepage/Shopifyplus-l.svg",
+          alt: "99minds & Shopify integration",
+          type: "image/svg",
+        },
+      ],
+      site_name: "99minds",
+    },
+    twitter: {
+      site: "@99minds_io",
+      cardType: "summary_large_image",
+    },
+  },
+  hero:{
+    logo: {
+    src: "/assets/shopify/99minds-Shopify.svg",
+    alt: "99minds and Shopify Integration",
+    width: 474,
+    height: 181,
+  },
+  heading: "Shopify Plus and 99minds Integration",
+  description:
+    "Countless Shopify stores trust 99minds for creating impactful Shopify loyalty programs and enhancing Shopify gift card experience. Integrate now to boost sales and customer retention.",
+  reviews: {
+    image: "/5-star.svg",
+    alt: "5 stars",
+    text: "5 Star Reviews",
+  },
+  cta: {
+    text: "Install on",
+    link: "https://apps.shopify.com/99minds-gift-card-loyalty-referral#",
+    logo: {
+      src: "/assets/shopify/Shopify-logo.svg",
+      alt: "Shopify logo",
+      width: 90,
+    },
+  },
+  showButtonsGrid: true,
+  buttons: [
+    { label: "Book Demo", link: "/demo" },
+    { label: "Learn More", link: "/learn" },
+  ],}
+};
