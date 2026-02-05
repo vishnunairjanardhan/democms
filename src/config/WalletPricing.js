b42e24ae988f1ca3b9947b5138dfd9a7c610fe93
vishnunairjanardhan <vjnair2001@gmail.com>
2026-02-05 17:55:42 +0530

    Minor changes in pricing

--- src/config/WalletPricing.js
+++ src/config/WalletPricing.js
@@ -1,7 +1,7 @@
 const pricingConfig = {
   title: "Get Started with 1,000 Free Active Passes",
-  subtitle: "1,000 Monthly active passes (MAPs) included in the free account. Additional passes cost only $0.05 per pass.",
-  description: "* Monthly active passes - Total number of new passes created or updated in a given billing cycle.",
+  subtitle: "1,000 Monthly Active Passes (MAPs) included in the free account. Additional passes cost only $0.05 per pass.",
+  description: "* Monthly Active Passes - Total number of new passes created or updated in a given billing cycle.",
 };
 
 export default pricingConfig;
