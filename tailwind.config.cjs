9454a4524e520518e095958ae73ccab4e6a3e8aa
vishnunairjanardhan <vjnair2001@gmail.com>
2026-03-05 11:59:26 +0530

    Revert "FCP render block fixes"
    This reverts commit c4124953e3a7099074cf83ec2239d889da9d81dd.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
     darkMode: ["class"],
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
   	fontSize: {
   		xs: [
