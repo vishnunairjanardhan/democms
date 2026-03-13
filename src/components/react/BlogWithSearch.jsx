762eeba39d133c0caeb45ca5f3dfd2567c6c5338
NikhilDandekar <nikhildandekar138@gmail.com>
2026-03-13 17:13:38 +0530

    Changes in Blog Tag Layout

--- src/components/react/BlogWithSearch.jsx
+++ src/components/react/BlogWithSearch.jsx
@@ -127,11 +127,7 @@ const BlogWithSearch = ({ sortedPosts = [], tags = [] }) => {
     <div className="flex flex-wrap">
 
       <div className="flex justify-center w-full">
-        <form
-          className="mt-10 sm:flex sm:max-w-md lg:w-1/2"
-          onSubmit={(e) => e.preventDefault()}
-        >
-          <label htmlFor="search" className="sr-only">Search Blog Posts</label>
+        <form className="mt-10 sm:flex sm:max-w-md lg:w-1/2 w-72">
           <input
             type="text"
             id="search"
