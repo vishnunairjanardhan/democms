969d50a5dee4af66d91869e06ef05ca699016389
NikhilDandekar <nikhildandekar138@gmail.com>
2026-03-13 17:13:38 +0530

    Changes in Blog Tag Layout

--- src/components/react/BlogWithSearch.jsx
+++ src/components/react/BlogWithSearch.jsx
@@ -115,7 +115,7 @@ const BlogWithSearch = ({ sortedPosts = [], tags = [] }) => {
 
       {/* Search */}
       <div className="flex justify-center w-full">
-        <form className="mt-10 sm:flex sm:max-w-md lg:w-1/2">
+        <form className="mt-10 sm:flex sm:max-w-md lg:w-1/2 w-72">
           <input
             type="text"
             id="search"
@@ -128,13 +128,13 @@ const BlogWithSearch = ({ sortedPosts = [], tags = [] }) => {
       </div>
 
       {/* Tags */}
-      <div className="mt-12 w-full">
-        <ul className="flex flex-wrap gap-2 justify-center">
+      <div className="lg:mt-12 mt-6 w-full overflow-x-auto scrollbar-hide">
+        <ul className="flex flex-nowrap lg:gap-3 gap-2 justify-start px-0">
           {tags.map((tag) => (
-            <li key={tag}>
+            <li key={tag} className="flex-shrink-0">
               <a
                 href={`/tags/${slugify(tag)}`}
-                className="flex items-center justify-center h-8 text-[#667085] text-sm px-4 py-2 font-semibold hover:bg-[#F9F5FF] hover:text-[#6941C6] rounded-lg"
+                className="flex items-center justify-center h-8 text-[#667085] text-sm px-3 py-2 font-semibold border hover:bg-[#F9F5FF] hover:text-[#6941C6] rounded-lg whitespace-nowrap"
               >
                 {tag}
               </a>
