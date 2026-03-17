ecf3db4f08e0413a7b220c5cdd5f2bf20112c0d3
NikhilDandekar <nikhildandekar138@gmail.com>
2026-03-17 17:08:59 +0530

    Changes in Blog page

--- src/components/react/BlogWithSearch.jsx
+++ src/components/react/BlogWithSearch.jsx
@@ -209,7 +209,7 @@ const scrollRight = () => {
                 href="/integrations"
                 className="px-4 py-2 font-semibold border border-gray-300 rounded-lg bg-white hover:bg-gray-100"
               >
-                Learn more
+                View all integrations
               </a>
 
               <a
